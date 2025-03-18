64b40fc37fdf1654fd784735181574ed8b390fc1
Marco Asta <marcoasta55@gmail.com>
2025-03-18 09:46:03 +0100

    feat - define events for toggle animation on layers

--- src/views/konva/components/NodeConnection.vue
+++ src/views/konva/components/NodeConnection.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+// Enum Imports
+import { EnumEvents } from '@/enums/business'
+
+// Utility Imports
+import { useEventBus } from '@vueuse/core'
+
 type Props = {
   x: number
   y: number
 }
 
 const props = defineProps<Props>()
+
+// Event Definition
+const evtToggleAnimation = useEventBus<IAnimation>(EnumEvents.ToggleAnimation)
 
 // Base rectangle configuration
 const baseRectConfig = {
@@ -15,10 +24,32 @@ const baseRectConfig = {
   stroke: 'black',
   fill: 'black',
 }
+
+evtToggleAnimation.on(() => {
+  animate()
+})
+
+// Animation Loop
+const animate = () => {
+  // Move slabs forward
+  // slabs.value.forEach((slab) => {
+  //   slab.x += 1
+  //   if (slab.x > 700) slab.x = 50 // Loop back
+  // })
+
+  // // Simulate burner flames pulsing
+  // burners.value.forEach((burner, index) => {
+  //   const scale = 1 + 0.1 * Math.sin(Date.now() / 200 + index)
+  //   burner.scaleX = scale
+  //   burner.scaleY = scale
+  // })
+
+  requestAnimationFrame(animate)
+}
 </script>
 
 <template>
-  <v-group :config="{ x , y }">
+  <v-group :config="{ x, y }">
     <!-- Base Rectangle -->
     <v-rect :config="baseRectConfig" />
   </v-group>
