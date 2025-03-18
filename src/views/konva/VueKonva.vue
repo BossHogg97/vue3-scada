1dcca0d469d8727d5fb0169023b17d355ef30c30
Marco Asta <marcoasta55@gmail.com>
2025-03-18 09:01:23 +0100

    feat - update [vue-konva] component

--- src/views/konva/VueKonva.vue
+++ src/views/konva/VueKonva.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 // Component Imports
 import ReheatingFurnace from './components/ReheatingFurnace.vue'
-import NodeConnection from './components/NodeConnection.vue';
+import NodeConnection from './components/NodeConnection.vue'
 
 // Utility Imports
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
+import { NButton } from 'naive-ui'
 
 // Start animation on mount
 onMounted(() => {
@@ -28,22 +29,32 @@ onMounted(() => {
 
 //   requestAnimationFrame(animate)
 // }
+
+const changeStatus = () => {}
 </script>
 
 <template>
-  <v-stage ref="stageRef" :config="{ width: 800, height: 400 }">
-    <v-layer>
-      <!-- First furnace at default position -->
-      <reheating-furnace :x="100" :y="50"  />
-    </v-layer>
+  <div class="flex flex-col">
+    <div class="border border-solid rounded-md shadow-sm">
+      <v-stage ref="stageRef" :config="{ width: 1100, height: 400 }">
+        <v-layer>
+          <!-- First furnace at default position -->
+          <reheating-furnace :x="100" :y="50" />
+        </v-layer>
 
-    <v-layer>
-      <node-connection :x="225" :y="70" />
-    </v-layer>
+        <v-layer>
+          <node-connection :x="225" :y="70" />
+        </v-layer>
 
-    <v-layer>
-      <!-- Second furnace moved to the right -->
-      <reheating-furnace :x="400" :y="50" />
-    </v-layer>
-  </v-stage>
+        <v-layer>
+          <!-- Second furnace moved to the right -->
+          <reheating-furnace :x="400" :y="50" />
+        </v-layer>
+      </v-stage>
+    </div>
+
+    <div class="w-30 mt-2">
+      <n-button type="primary" @click="changeStatus">Change Status</n-button>
+    </div>
+  </div>
 </template>
